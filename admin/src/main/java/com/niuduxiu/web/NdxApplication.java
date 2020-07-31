835345d53b8499db966865c5a2a4019a3d72586b
guojianhui <guojianhui@qq.com>
2020-07-31 18:00:33 +0800

    first commit

---
+++ admin/src/main/java/com/niuduxiu/web/NdxApplication.java
@@ -0,0 +1,11 @@
+package com.niuduxiu.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NdxApplication.class, args);
+    }
+}
---
+++ admin/src/main/java/com/niuduxiu/web/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.niuduxiu.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping("/index")
+    public String Index() {
+        return "index";
+    }
+}
